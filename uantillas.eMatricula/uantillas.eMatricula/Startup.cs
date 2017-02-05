3160fa1be443558365fa1258272f5000d2be3e7c
CarlosVidal Mora <carlosvidal.mora@prodigious.cr>
2017-02-05 12:29:29 -0600

    first commit

---
+++ uantillas.eMatricula/uantillas.eMatricula/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(uantillas.eMatricula.Startup))]
+namespace uantillas.eMatricula
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
