e201ae38c41e44e8b926011d75a6a9011f438346
cavimora <cavimora@gmail.com>
2017-04-01 09:31:10 -0600

    commit

---
+++ eMat.Web/eMat.Web/Controllers/GruposController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using eMat.DA;
+
+namespace eMat.Web.Controllers
+{
+    public class GruposController : Controller
+    {
+        private eMatriculaEntities db = new eMatriculaEntities();
+
+        // GET: tbGrupoes
+        public async Task<ActionResult> Index()
+        {
+            var tbGrupo = db.tbGrupo.Include(t => t.tbCurso);
+            return View(await tbGrupo.ToListAsync());
+        }
+
+        // GET: tbGrupoes/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbGrupo tbGrupo = await db.tbGrupo.FindAsync(id);
+            if (tbGrupo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbGrupo);
+        }
+
+        // GET: tbGrupoes/Create
+        public ActionResult Create()
+        {
+            ViewBag.idCurso = new SelectList(db.tbCurso, "idCurso", "nombre");
+            return View();
+        }
+
+        // POST: tbGrupoes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "idGrupo,idCurso,horario")] tbGrupo tbGrupo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tbGrupo.Add(tbGrupo);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.idCurso = new SelectList(db.tbCurso, "idCurso", "nombre", tbGrupo.idCurso);
+            return View(tbGrupo);
+        }
+
+        // GET: tbGrupoes/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbGrupo tbGrupo = await db.tbGrupo.FindAsync(id);
+            if (tbGrupo == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.idCurso = new SelectList(db.tbCurso, "idCurso", "nombre", tbGrupo.idCurso);
+            return View(tbGrupo);
+        }
+
+        // POST: tbGrupoes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "idGrupo,idCurso,horario")] tbGrupo tbGrupo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tbGrupo).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.idCurso = new SelectList(db.tbCurso, "idCurso", "nombre", tbGrupo.idCurso);
+            return View(tbGrupo);
+        }
+
+        // GET: tbGrupoes/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbGrupo tbGrupo = await db.tbGrupo.FindAsync(id);
+            if (tbGrupo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbGrupo);
+        }
+
+        // POST: tbGrupoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            tbGrupo tbGrupo = await db.tbGrupo.FindAsync(id);
+            db.tbGrupo.Remove(tbGrupo);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ eMat.Web/eMat.Web/Controllers/MatriculaController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using eMat.DA;
+
+namespace eMat.Web.Controllers
+{
+    public class MatriculaController : Controller
+    {
+        private eMatriculaEntities db = new eMatriculaEntities();
+
+        // GET: Matricula
+        public async Task<ActionResult> Index()
+        {
+            var tbMatricula = db.tbMatricula.Include(t => t.tbEstudiante).Include(t => t.tbGrupo);
+            return View(await tbMatricula.ToListAsync());
+        }
+
+        // GET: Matricula/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbMatricula tbMatricula = await db.tbMatricula.FindAsync(id);
+            if (tbMatricula == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbMatricula);
+        }
+
+        // GET: Matricula/Create
+        public ActionResult Create()
+        {
+            ViewBag.carnet = new SelectList(db.tbEstudiante, "carnet", "cedula");
+            ViewBag.idGrupo = new SelectList(db.tbGrupo, "idGrupo", "idCurso");
+            return View();
+        }
+
+        // POST: Matricula/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "carnet,idGrupo,cuatrimestre,anio,nota")] tbMatricula tbMatricula)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tbMatricula.Add(tbMatricula);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.carnet = new SelectList(db.tbEstudiante, "carnet", "cedula", tbMatricula.carnet);
+            ViewBag.idGrupo = new SelectList(db.tbGrupo, "idGrupo", "idCurso", tbMatricula.idGrupo);
+            return View(tbMatricula);
+        }
+
+        // GET: Matricula/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbMatricula tbMatricula = await db.tbMatricula.FindAsync(id);
+            if (tbMatricula == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.carnet = new SelectList(db.tbEstudiante, "carnet", "cedula", tbMatricula.carnet);
+            ViewBag.idGrupo = new SelectList(db.tbGrupo, "idGrupo", "idCurso", tbMatricula.idGrupo);
+            return View(tbMatricula);
+        }
+
+        // POST: Matricula/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "carnet,idGrupo,cuatrimestre,anio,nota")] tbMatricula tbMatricula)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tbMatricula).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.carnet = new SelectList(db.tbEstudiante, "carnet", "cedula", tbMatricula.carnet);
+            ViewBag.idGrupo = new SelectList(db.tbGrupo, "idGrupo", "idCurso", tbMatricula.idGrupo);
+            return View(tbMatricula);
+        }
+
+        // GET: Matricula/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbMatricula tbMatricula = await db.tbMatricula.FindAsync(id);
+            if (tbMatricula == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbMatricula);
+        }
+
+        // POST: Matricula/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            tbMatricula tbMatricula = await db.tbMatricula.FindAsync(id);
+            db.tbMatricula.Remove(tbMatricula);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
